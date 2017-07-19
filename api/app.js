004d7a8fa24ef3f80dd1ec2211028f884bcede07
Daniel Botero <dbotero@matisses.co>
2017-07-19 09:25:21 -0500

    Revert "Pendientes"
    This reverts commit 3e4bcb237baf105d295ef93476cfeb032bb853b8.

--- api/app.js
+++ api/app.js
@@ -5,11 +5,24 @@ var bodyParser = require('body-parser');
 var app = express();
 
 var itemRoutes = require('./routes/item');
+var menuItemRoutes = require('./routes/menu-item');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(bodyParser.json());
 
-app.use('/api', itemRoutes);
+//configurar cabeceras http
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Access', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
+
+app.use('/api/item', itemRoutes);
+app.use('/api/menuitem', menuItemRoutes);
 
 module.exports = app;
-
