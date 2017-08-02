6cebee6e3607e14f674bdc71a805e32392994c0c
jguisao <jguisao@matisses.co>
2017-08-02 11:32:41 -0500

    Validación Información pago

--- client/src/app/components/info-pago/info-pago.component.ts
+++ client/src/app/components/info-pago/info-pago.component.ts
@@ -23,10 +23,12 @@ export class InfoPagoComponent implements OnInit {
   private carrito: CarritoSimpleComponent;
 
   public totalEnvio: number = 0;
+  public messageError: string = '';
+  public procesandoP2P: boolean = false;
+  public valid: boolean = true;
   public customer: Customer;
   public ciudadesPrincipales: Array<City>;
   public otrasCiudades: Array<City>;
-  public procesandoP2P: boolean = false;
 
   constructor(private _route: ActivatedRoute, private _router: Router, private _customerService: CustomerService, private _cityService: CityService) {
     this.ciudadesPrincipales = new Array<City>();
@@ -50,6 +52,7 @@ export class InfoPagoComponent implements OnInit {
   ngOnInit() {
     console.log('inicializando componente de información de pago');
     this.carrito.cargarCarrito();
+    this.obtenerCiudades();
   }
 
   public obtenerCiudades() {
@@ -74,9 +77,31 @@ export class InfoPagoComponent implements OnInit {
   }
 
   public buscarCliente() {
+    this.customer.fiscalID = this.customer.fiscalID.trim();
+    this.messageError = '';
     if (this.customer.fiscalID != null && this.customer.fiscalID.length > 0) {
       this._customerService.getCustomerData(this.customer.fiscalID).subscribe(
         response => {
+          if (response.fiscalIdType == '31') {
+            console.log('El documento no es válido para facturar por este medio: ' + response.fiscalID + ' Tipo Doc: ' + response.fiscalIdType + '-Nit');
+            this.messageError = 'Este documento no es válido para facturar por este sitio, esta registrado actualmente como NIT.';
+            this.customer = this.customer = new Customer().newCustomer('', '', null, '', '', '', '', '', '', '', '', null, '', '', '', '', '', '', [{
+              stateCode: null,
+              stateName: null,
+              cityCode: null,
+              cityName: null,
+              addressName: null,
+              addressType: null,
+              address: null,
+              landLine: null,
+              cellphone: null,
+              email: null,
+              country: null,
+              taxCode: null
+            }]);
+            this.valid = false;
+            return;
+          }
           this.customer = response;
           console.log(response);
         },
@@ -88,9 +113,13 @@ export class InfoPagoComponent implements OnInit {
   }
 
   public pagar() {
-    if (this.customer.cardCode == null || this.customer.cardCode.length <= 0 || this.customer.firstName == null || this.customer.firstName.length <= 0
+    this.valid = true;
+    this.messageError = '';
+    if (this.customer.fiscalID == null || this.customer.cardCode.length <= 0 || this.customer.firstName == null || this.customer.firstName.length <= 0
       || this.customer.lastName1 == null || this.customer.lastName1.length <= 0) {
       console.log('Se deben llenar todos los campos obligatorios para poder proceder con el pago');
+      this.messageError = 'Se deben llenar todos los campos obligatorios para poder proceder con el pago.';
+      this.valid = false;
       return;
     }
     console.log('El usuario sera redireccionado a PlacetoPay');
