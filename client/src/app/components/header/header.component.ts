244caa7919e1880204a49599088bfec53ddb8833
Daniel Botero <dbotero@matisses.co>
2017-08-10 10:02:57 -0500

    Correcion modal carrito

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,18 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 
+import { Item } from '../../models/item';
+
+declare var $: any;
+
 @Component({
-    selector: 'matisses-header',
-    templateUrl: 'header.html',
-    styleUrls: ['header.component.css']
+  selector: 'matisses-header',
+  templateUrl: 'header.html',
+  styleUrls: ['header.component.css']
 })
 
 export class HeaderComponent implements OnInit {
+  public lastAddedItem: Item;
+  constructor(private _route: ActivatedRoute, private _router: Router) {
+  }
 
-    constructor(private _route: ActivatedRoute, private _router: Router) {
-    }
+  ngOnInit() {
+    console.log('configurando accion antes de mostrar modal');
+    $('#carritoModal').on('show.bs.modal', () => {
+      this.cargarInfoModal();
+    });
+  }
 
-    ngOnInit() {
-        //console.log('inicializando componente de encabezado');
-    }
+  public cargarInfoModal() {
+    console.log('cargando info modal carrito');
+    this.lastAddedItem = JSON.parse(localStorage.getItem('matisses.lastAddedItem'));
+    console.log(this.lastAddedItem);
+    localStorage.removeItem('matisses.lastAddedItem');
+  }
 }
