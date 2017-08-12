d9e1153cfe202c507b1793f18b9def73306fd1ea
Daniel Botero <dbotero@matisses.co>
2017-08-12 13:05:21 -0500

    Actualizacion puerto wildfly

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,6 +1,6 @@
 export let GLOBAL = {
     url: 'http://api.matisses.co:3977/api/',
-    urlBCS: 'http://api.matisses.co:8080/BCS/res/',
+    urlBCS: 'http://api.matisses.co:8180/BCS/res/',
     recaptchaSiteKey: '6Ld3UiwUAAAAAPqMjEImY8qg5YlZUlu0GuDmyJGr',
     toMail: 'sistemas@matisses.co, mercadeo@matisses.co'
 };
