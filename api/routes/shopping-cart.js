2144ca31c04dc77ebfa20fed6bfa62b136276e51
Yeison Gil <ygil@matisses.co>
2017-08-16 16:26:37 -0500

    Cambio de ruta para el retorno y errores de place to pay

--- api/routes/shopping-cart.js
+++ api/routes/shopping-cart.js
@@ -6,6 +6,6 @@ var ShoppingCartController = require('../controllers/shopping-cart');
 var api = express.Router();
 
 api.post('/guardar/', ShoppingCartController.saveShoppingCart);
-api.get('/buscar/:id', ShoppingCartController.findShoppingCart);
+api.get('/buscar/:id/:origin?', ShoppingCartController.findShoppingCart);
 
 module.exports = api;
--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,6 +1,7 @@
 export let GLOBAL = {
-    url: 'http://api.matisses.co:3977/api/',
-    urlBCS: 'http://api.matisses.co:8180/BCS/res/',
-    recaptchaSiteKey: '6Ld3UiwUAAAAAPqMjEImY8qg5YlZUlu0GuDmyJGr',
-    toMail: 'sistemas@matisses.co, mercadeo@matisses.co'
+  url: 'http://api.matisses.co:3977/api/',
+  urlBCS: 'http://api.matisses.co:8180/BCS/res/',
+  recaptchaSiteKey: '6Ld3UiwUAAAAAPqMjEImY8qg5YlZUlu0GuDmyJGr',
+  toMail: 'sistemas@matisses.co, mercadeo@matisses.co',
+  urlTransactionResult: 'http://192.168.5.23:4200/resultado-transaccion/'
 };
