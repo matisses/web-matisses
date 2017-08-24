652baaaaae6f9ee9b23e23f2f5d2402ba4d4f48e
Yeison Gil <ygil@matisses.co>
2017-08-24 08:28:22 -0500

    https en api

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,6 +1,5 @@
 export let GLOBAL = {
-  url: 'http://api.matisses.co:3977/api/',
-  //urlBCS: 'http://api.matisses.co:8080/BCS/res/',
+  url: 'https://api.matisses.co:3977/api/',
   urlBCS: 'http://hercules.matisses.co:8080/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'sistemas@matisses.co, mercadeo@matisses.co',
