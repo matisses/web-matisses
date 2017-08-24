7a2f8894eb0e81df1575d0adeb1c09d5b1cce086
Yeison Gil <ygil@matisses.co>
2017-08-24 10:22:09 -0500

    Corrección de rutas a https

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,7 +1,7 @@
 export let GLOBAL = {
   url: 'https://api.matisses.co:3977/api/',
-  urlBCS: 'http://hercules.matisses.co:8080/BCS/res/',
+  urlBCS: 'https://360.matisses.co:8443/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'sistemas@matisses.co, mercadeo@matisses.co',
-  urlTransactionResult: 'http://www.matisses.co/resultado-transaccion/'
+  urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/'
 };
