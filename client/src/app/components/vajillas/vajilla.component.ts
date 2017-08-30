24067e0b7354d98907a973a110270b26db1e97ef
Alejandro <aguerra@matisses.co>
2017-08-30 15:23:31 -0500

    Componente de vajillas con boton de adminsitrador

--- client/src/app/components/vajillas/vajilla.component.ts
+++ client/src/app/components/vajillas/vajilla.component.ts
@@ -1,34 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-
+import { ItemService } from '../../services/item.service';
 import { Item } from '../../models/item';
 
 declare var $: any;
 
 @Component({
     templateUrl: 'vajilla.html',
-    styleUrls: ['vajilla.component.css']
+    styleUrls: ['vajilla.component.css'],
+    providers: [ItemService]
 })
 
 export class VajillaComponent implements OnInit {
-    public title: string;
     public items:Array<Item>;
+    public puesto:Array<Item>;
 
-    constructor(private _route: ActivatedRoute, private _router: Router) {
-        this.title = 'Modulo de vajillas';
+    constructor(private _route: ActivatedRoute, private _router: Router, private _itemService: ItemService) {
     }
 
     ngOnInit() {
         this.inicializarItems();
+        this.inicializarPuesto();
     }
 
     private inicializarItems(){
 
       this.items = new Array<Item>();
-      this.items.push(new Item().newItem('22600000000000000070', 'Domo Gold', 56000));
-      this.items.push(new Item().newItem('22600000000000000071', 'Domo Platinum', 56000));
-      this.items.push(new Item().newItem('22600000000000000072', 'Domo White', 400000));
-      this.items.push(new Item().newItem('22600000000000000073', 'A la Plage', 89000));
+      this.items.push(new Item().newItem('1', 'Vajilla 4 puestos', 56000));
+      this.items.push(new Item().newItem('2', 'Vajilla 4 puestos', 56000));
+      this.items.push(new Item().newItem('3', 'Vajilla 4 puestos', 400000));
+    }
+
+    private inicializarPuesto() {
+      this.puesto = new Array<Item>();
+      this._itemService.find('2260041').subscribe(
+        response => {
+          this.puesto.push(response.result[0]);
+          this._itemService.find('2260045').subscribe(
+            response => {
+              this.puesto.push(response.result[0]);
+              this._itemService.find('2260043').subscribe(
+                response => {
+                  this.puesto.push(response.result[0]);
+                  this._itemService.find('2260276').subscribe(
+                    response => {
+                      this.puesto.push(response.result[0]);
+                    }, error => { console.error(); }
+                  );
+                }, error => { console.error(); }
+              );
+            }, error => { console.error(); }
+          );
+        }, error => { console.error(); }
+      );
     }
 
 }
