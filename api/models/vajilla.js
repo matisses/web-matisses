f2c2a6bad904636dbd1243f72a97da27fc8ad542
jguisao <jguisao@matisses.co>
2017-09-19 15:36:02 -0500

    Nuevo campo en vajillas

---
+++ api/models/vajilla.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CrockerySchema = Schema({
+    name: String,
+    brand: String,
+    coleccion: String,
+    photo: String,
+    price: Number,
+    items: Number,
+    pieces: Number,
+    detail: [
+      {
+        itemcode: String,
+        quantity: Number
+      }
+    ]
+});
+
+module.exports = mongoose.model('crockery', CrockerySchema);
