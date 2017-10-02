dd8290e2e8b14ba711e23871d6ab3f1940b58e7a
Alejandro <aguerra@matisses.co>
2017-10-02 17:18:41 -0500

    Arreglos para productos verdi

--- client/src/app/components/principal/slide1/slide-1.component.ts
+++ client/src/app/components/principal/slide1/slide-1.component.ts
@@ -32,7 +32,7 @@ export class Slide1Component implements OnInit {
         this._itemService.find('2580004').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('2580008').subscribe(
+            this._itemService.find('2580005').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }
