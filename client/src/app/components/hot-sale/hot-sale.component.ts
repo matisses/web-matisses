d6e87a703e93c5f333f4179b7d2007d5c83cd760
Alejandro <aguerra@matisses.co>
2017-11-14 17:36:01 -0500

    micrositio hot sale

--- client/src/app/components/hot-sale/hot-sale.component.ts
+++ client/src/app/components/hot-sale/hot-sale.component.ts
@@ -73,6 +73,66 @@ export class HotSaleComponent implements OnInit {
                                                                 this._itemService.find('2420038').subscribe(// Item 16
                                                                   response => {
                                                                     this.items.push(response.result[0]);
+                                                                    this._itemService.find('2310205').subscribe(// Item 17
+                                                                      response => {
+                                                                        this.items.push(response.result[0]);
+                                                                        this._itemService.find('2310206').subscribe(// Item 18
+                                                                          response => {
+                                                                            this.items.push(response.result[0]);
+                                                                            this._itemService.find('2310208').subscribe(// Item 19
+                                                                              response => {
+                                                                                this.items.push(response.result[0]);
+                                                                                this._itemService.find('2310209').subscribe(// Item 20
+                                                                                  response => {
+                                                                                    this.items.push(response.result[0]);
+                                                                                    this._itemService.find('2220524').subscribe(// Item 21
+                                                                                      response => {
+                                                                                        this.items.push(response.result[0]);
+                                                                                        this._itemService.find('2220543').subscribe(// Item 22
+                                                                                          response => {
+                                                                                            this.items.push(response.result[0]);
+                                                                                            this._itemService.find('2220531').subscribe(// Item 23
+                                                                                              response => {
+                                                                                                this.items.push(response.result[0]);
+                                                                                                this._itemService.find('2220526').subscribe(// Item 24
+                                                                                                  response => {
+                                                                                                    this.items.push(response.result[0]);
+                                                                                                    this._itemService.find('2270029').subscribe(// Item 25
+                                                                                                      response => {
+                                                                                                        this.items.push(response.result[0]);
+                                                                                                        this._itemService.find('2270018').subscribe(// Item 26
+                                                                                                          response => {
+                                                                                                            this.items.push(response.result[0]);
+                                                                                                            this._itemService.find('2270060').subscribe(// Item 27
+                                                                                                              response => {
+                                                                                                                this.items.push(response.result[0]);
+                                                                                                                this._itemService.find('2270020').subscribe(// Item 28
+                                                                                                                  response => {
+                                                                                                                    this.items.push(response.result[0]);
+                                                                                                                  }, error => { console.error(); }
+                                                                                                                );
+                                                                                                              }, error => { console.error(); }
+                                                                                                            );
+                                                                                                          }, error => { console.error(); }
+                                                                                                        );
+                                                                                                      }, error => { console.error(); }
+                                                                                                    );
+                                                                                                  }, error => { console.error(); }
+                                                                                                );
+                                                                                              }, error => { console.error(); }
+                                                                                            );
+                                                                                          }, error => { console.error(); }
+                                                                                        );
+                                                                                      }, error => { console.error(); }
+                                                                                    );
+                                                                                  }, error => { console.error(); }
+                                                                                );
+                                                                              }, error => { console.error(); }
+                                                                            );
+                                                                          }, error => { console.error(); }
+                                                                        );
+                                                                      }, error => { console.error(); }
+                                                                    );
                                                                   }, error => { console.error(); }
                                                                 );
                                                               }, error => { console.error(); }
