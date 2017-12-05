462d6ddc0aa6fbe01ddb8967aafe67bc8013661a
Alejandro <aguerra@matisses.co>
2017-12-05 10:23:07 -0500

    Banner de navidad

--- client/src/app/components/principal/slide4/slide-4.component.ts
+++ client/src/app/components/principal/slide4/slide-4.component.ts
@@ -26,10 +26,10 @@ export class Slide4Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('2090083').subscribe(
+    this._itemService.find('2290239').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2090040').subscribe(
+        this._itemService.find('2290023').subscribe(
           response => {
             this.items.push(response.result[0]);
             this._itemService.find('2090093').subscribe(
