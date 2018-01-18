0abf63e58f8fe5c00549d16d970b7140d2578696
jguisao <jguisao@matisses.co>
2018-01-18 09:39:23 -0500

    Subiendo un cambio para probar branch

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -2,6 +2,6 @@ export let GLOBAL = {
   url: 'https://api.matisses.co:3977/api/',
   urlBCS: 'https://360.matisses.co:8443/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
-  toMail: 'info@matisses.co',
+  toMail: 'sistemas@matisses.co',
   urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/'
 };
