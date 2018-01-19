01da4f3e29ec01c37b66d6a863ca91e6456d3737
Alejandro <aguerra@matisses.co>
2018-01-19 07:43:55 -0500

    Banner cuchillos WMF

--- client/src/app/components/principal/slide2/slide-2.component.ts
+++ client/src/app/components/principal/slide2/slide-2.component.ts
@@ -26,13 +26,13 @@ export class Slide2Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('2230007').subscribe(
+    this._itemService.find('2310654').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2230019').subscribe(
+        this._itemService.find('2310428').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('2230019').subscribe(
+            this._itemService.find('2310456').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }
