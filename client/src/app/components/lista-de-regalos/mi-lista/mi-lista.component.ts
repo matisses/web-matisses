d2b9ceb19ddc6e567287ac16bac7dc5f99a25c78
Alejandro <aguerra@matisses.co>
2018-01-23 10:06:01 -0500

    Actualizacion mi lista

--- client/src/app/components/lista-de-regalos/mi-lista/mi-lista.component.ts
+++ client/src/app/components/lista-de-regalos/mi-lista/mi-lista.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ItemService } from '../../../services/item.service';
+import { Item } from '../../../models/item';
 
 
 declare var jquery: any;
@@ -7,33 +9,44 @@ declare var $: any;
 
 @Component({
   templateUrl: 'mi-lista.html',
-  styleUrls: ['mi-lista.component.css']
+  styleUrls: ['mi-lista.component.css'],
+  providers: [ItemService]
 })
 
 export class MiListaComponent implements OnInit {
   public nombreUsuario: string;
-  public claveNueva:string;
-  public claveConfirmacion:string;
+  public claveNueva: string;
+  public claveConfirmacion: string;
   public messageError: string;
+  public items: Array<Item>;
 
-  constructor(private _route: ActivatedRoute, private _router: Router) {
+  constructor(private _route: ActivatedRoute, private _router: Router, private _itemService: ItemService) {
     this.nombreUsuario = localStorage.getItem('username-lista');
   }
 
   ngOnInit() {
-    if(localStorage.getItem('cambio-clave')=='si'){
+    if (localStorage.getItem('cambio-clave') == 'si') {
       $('#cambioContraseña').modal('show');
     }
   }
 
 
   ngAfterViewInit() {
+    this.inicializarItems();
+  }
+
+  public actualizarClave() {
+
 
   }
 
-  public actualizarClave(){
-
-
+  private inicializarItems() {
+    this.items = new Array<Item>();
+    this._itemService.find('2280058').subscribe( // Item 1
+      response => {
+        this.items.push(response.result[0]);
+      }, error => { console.error(); }
+    );
   }
 
 }
