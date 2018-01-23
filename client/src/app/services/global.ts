c2ea06cd564d9722a81b5dbb0a3d0efe509785ee
mperdomo83 <mperdomo@matisses.co>
2018-01-23 17:14:17 -0500

    conflicto

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,6 +1,6 @@
 export let GLOBAL = {
   url: 'https://api.matisses.co:3977/api/',
-  urlBCS: 'https://360.matisses.co:8443/BCS/res/',
+  urlBCS: 'http://192.168.5.152:8080/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'info@matisses.co',
   urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/'
