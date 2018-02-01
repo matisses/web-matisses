01b4e688923f6936127cdb2058b7bc723016d445
Alejandro <aguerra@matisses.co>
2018-02-01 18:39:11 -0500

    arreglo *******************************

--- client/src/app/models/datos-pago-placetopay.ts
+++ client/src/app/models/datos-pago-placetopay.ts
@@ -36,8 +36,33 @@ export class DatosPagoPlaceToPay {
   public expiration: string;
   public returnUrl: string;
   public ipAddress: string;
+  public codigoLista: string;
 
   constructor() {
+  }
+
+  public newDatosPagoPlaceToPayLista(
+    buyer: { document: string, name: string, surname: string, documentType: string, email: string, mobile: string, address: { street: string, city: string, country: string } },
+    locale: string,
+    userAgent: string,
+    payment: { allowPartial: string, reference: string, description: string, amount: { currency: string, total: number, taxes: { kind: string, amount: number } } },
+    auth: { login: string, seed: string, nonce: string, tranKey: string },
+    expiration: string,
+    returnUrl: string,
+    ipAddress: string,codigoLista:string) {
+    this.buyer = buyer;
+    if (locale != null) {
+      this.locale = locale;
+    }
+    this.userAgent = userAgent;
+    this.payment = payment;
+    this.auth = auth;
+    this.expiration = expiration;
+    this.returnUrl = returnUrl;
+    this.ipAddress = ipAddress;
+    this.codigoLista=codigoLista;
+
+    return this;
   }
 
   public newDatosPagoPlaceToPay(
@@ -59,6 +84,7 @@ export class DatosPagoPlaceToPay {
     this.expiration = expiration;
     this.returnUrl = returnUrl;
     this.ipAddress = ipAddress;
+
 
     return this;
   }
