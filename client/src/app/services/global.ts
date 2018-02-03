3a78f13ae0da4d5b3ca4e45ffb333e7e9cd54ad5
jguisao <jguisao@matisses.co>
2018-02-03 10:28:25 -0500

    subiendo url de transaccion de lista de regalo

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -3,5 +3,6 @@ export let GLOBAL = {
   urlBCS: 'https://360.matisses.co:8443/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'info@matisses.co',
-  urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/'
+  urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/',
+  urlTransactionResultList: 'https://www.matisses.co/resultado-transaccion-regalos/'
 };
