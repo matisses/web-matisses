8fa32c8824027d4d26bcedac99fd2823a6b2141f
Alejandro <aguerra@matisses.co>
2018-02-20 17:33:26 -0500

    Beneficios 02

--- client/src/app/components/lista-de-regalos/beneficios/beneficios.component.ts
+++ client/src/app/components/lista-de-regalos/beneficios/beneficios.component.ts
@@ -21,6 +21,28 @@ export class BeneficiosComponent implements OnInit {
   }
 
   ngAfterViewInit() {
+    $(".slider-aliados").slick({
+      prevArrow: '.slider-container .prev',
+      nextArrow: '.slider-container .next',
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      responsive: [
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
   }
 
 }
