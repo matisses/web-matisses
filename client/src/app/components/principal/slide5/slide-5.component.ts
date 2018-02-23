2e326468db21f0fe13703b7b977337c9d43efb14
Alejandro <aguerra@matisses.co>
2018-02-23 11:41:02 -0500

    Banner de mantenimiento

---
+++ client/src/app/components/principal/slide5/slide-5.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ItemService } from '../../../services/item.service';
+import { Item } from '../../../models/item';
+
+//declare var jquery: any;
+declare var $: any;
+
+@Component({
+  selector: 'slide-5',
+  templateUrl: 'slide-5.html',
+  styleUrls: ['slide-5.component.css'],
+  providers: [ItemService]
+})
+
+export class Slide5Component implements OnInit {
+  public items: Array<Item>;
+
+  constructor(private _route: ActivatedRoute, private _router: Router, private _itemService: ItemService) {
+
+  }
+
+  ngOnInit() {
+    this.inicializarItems();
+  }
+
+  private inicializarItems() {
+    this.items = new Array<Item>();
+    this._itemService.find('2640001').subscribe(
+      response => {
+        this.items.push(response.result[0]);
+        this._itemService.find('2090109').subscribe(
+          response => {
+            this.items.push(response.result[0]);
+            this._itemService.find('2440081').subscribe(
+              response => {
+                this.items.push(response.result[0]);
+              }, error => { console.error(); }
+            );
+          }, error => { console.error(); }
+        );
+      }, error => { console.error(); }
+    );
+  }
+}
