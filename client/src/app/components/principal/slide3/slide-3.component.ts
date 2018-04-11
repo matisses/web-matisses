af17a1f93a4dd0158d7ecacf4abf3b444eddf5ae
Alejandro <aguerra@matisses.co>
2018-04-11 11:50:00 -0500

    Actualizacion de banner

--- client/src/app/components/principal/slide3/slide-3.component.ts
+++ client/src/app/components/principal/slide3/slide-3.component.ts
@@ -26,13 +26,13 @@ export class Slide3Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('1051844').subscribe(
+    this._itemService.find('2410007').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('1051825').subscribe(
+        this._itemService.find('2410080').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('2210051').subscribe(
+            this._itemService.find('2380285').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }
