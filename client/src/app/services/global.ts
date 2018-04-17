ef7aebe4a8dbc3ee8ffb94d104af6a0d09b13490
jguisao <jguisao@matisses.co>
2018-04-17 15:49:56 -0500

    Corregir global

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,7 +1,6 @@
 export let GLOBAL = {
   url: 'https://api.matisses.co:8081/api/',
-  //urlBCS: 'https://360.matisses.co:8443/BCS/res/',
-  urlBCS: 'http://192.168.5.157:8080/BCS/res/',
+  urlBCS: 'https://360.matisses.co:8443/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'info@matisses.co',
   urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/',
