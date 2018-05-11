cfc0719d7b6ea0582ef969ede4c3b74ed442c8aa
Alejandro <aguerra@matisses.co>
2018-05-11 13:55:44 -0500

    Global

--- client/src/app/services/global.ts
+++ client/src/app/services/global.ts
@@ -1,7 +1,7 @@
 export let GLOBAL = {
   url: 'https://api.matisses.co:8081/api/',
-  // urlBCS: 'https://360.matisses.co:8443/BCS/res/',
-  urlBCS: 'http://192.168.5.155:8080/BCS/res/',
+  urlBCS: 'https://360.matisses.co:8443/BCS/res/',
+  // urlBCS: 'http://192.168.5.155:8080/BCS/res/',
   recaptchaSiteKey: '6LekJy0UAAAAANoeggt1-X3VqaROvs97RZI8g3Co',
   toMail: 'info@matisses.co',
   urlTransactionResult: 'https://www.matisses.co/resultado-transaccion/',
