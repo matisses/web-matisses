a0d13156cb09dc7b4786f5503b655a5c8efb8f0c
Alejandro <aguerra@matisses.co>
2018-07-03 10:55:41 -0500

    cambio de bannner

--- client/src/app/components/principal/slide2/slide-2.component.ts
+++ client/src/app/components/principal/slide2/slide-2.component.ts
@@ -26,13 +26,13 @@ export class Slide2Component implements OnInit {
 
   private inicializarItems() {
     this.items = new Array<Item>();
-    this._itemService.find('2270015').subscribe(
+    this._itemService.find('2220183').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2240010').subscribe(
+        this._itemService.find('2220198').subscribe(
           response => {
             this.items.push(response.result[0]);
-            this._itemService.find('1760005').subscribe(
+            this._itemService.find('2220175').subscribe(
               response => {
                 this.items.push(response.result[0]);
               }, error => { console.error(); }
