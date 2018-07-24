99c232e99ae2a0c71f23b42197f298c13830779e
Alejandro <aguerra@matisses.co>
2018-07-24 15:25:39 -0500

    Corrección Banne ppl

--- client/src/app/components/principal/slide6/slide-6.component.ts
+++ client/src/app/components/principal/slide6/slide-6.component.ts
@@ -29,7 +29,7 @@ export class Slide6Component implements OnInit {
     this._itemService.find('2440088').subscribe(
       response => {
         this.items.push(response.result[0]);
-        this._itemService.find('2090099').subscribe(
+        this._itemService.find('2090120').subscribe(
           response => {
             this.items.push(response.result[0]);
             this._itemService.find('1051486').subscribe(
